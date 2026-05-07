812d7b34af7633aa500cac252200e276d3e27f91
sagarrytclick-a11y <sagar.rytclick@gmail.com>
2026-05-07 11:38:39 +0530

    Update ContactPopup.tsx
    small chnaged

--- components/ContactPopup.tsx
+++ components/ContactPopup.tsx
@@ -49,7 +49,7 @@ const ContactPopup: React.FC = () => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
+    <div className="fixed inset-0 z-60 flex items-center justify-center p-4">
       {/* Backdrop */}
       <div 
         className="absolute inset-0  bg-opacity-50 backdrop-blur-sm"
